5aad22765d21a7c73112f6374d9cf598b5da217c
Flora <flora.boularan@jack.agency>
2023-01-27 13:33:59 +0100

    grid

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <button class="bg-red-500 text-white">coucou</button>
+  <div class="container mx-auto w-96 grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 bg-red">
+    <div>
+      <img src="./assets/img/dragonTea.jpg" alt="">
+    </div>
+    <div>
+      <img src="./assets/img/oiseau-couleur.jpg" alt="">
+    </div>
+    <div>
+      <img src="./assets/img/LesVolants2.jpg" alt="">
+    </div>
+    <div>
+      <img src="./assets/img/Maxine.png" alt="">
+    </div>
+    <div>
+      <img src="./assets/img/charlie.png" alt="">
+    </div>
+      
+    
   </div>
 </template>
