0439f3d6d882bd167b50cde0d6af18a49986aff9
Flora <flora.boularan@jack.agency>
2023-02-14 14:25:37 +0100

    wip grid portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -6,12 +6,8 @@
         <img class="w-96" src="../assets/img/visuel.png" alt="">
     </div> -->
 
-    <section class="flex justify-between">
-      <div class="w-1/4 border-t-2"></div>
-      <div class="w-1/3"></div>
-      <div class="w-1/4 border-t-2"></div>
-    </section>
-  
+
+
     <!-- <div class="container max-w-xl lg:max-w-4xl mx-auto px-4 py-8 grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-2">
       <div>
         <img src="../assets/img2/LeBoule.jpg" alt="">
@@ -47,6 +43,7 @@
 
 
       <div class="container max-w-xl lg:max-w-4xl mx-auto px-4 py-8 gap-y-10 gap-x-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+     
       <div>
         <img src="../assets/img2/LeBoule.jpg" alt="">
       </div>
@@ -56,35 +53,90 @@
       <div>
         <img src="../assets/img2/Lamoula.jpg" alt="">
       </div>
+
+ 
       <!-- <div>
+        <img src="../assets/img2/lenvolée-cadre.jpg" alt="">
+      </div>
+      <div class="sm:col-start-3">
+        <img src="../assets/img2/perroquet-cadre.jpg" alt="">
+      </div> -->
+
+      
+      <!-- <div>
+        <img src="../assets/img2/beetleJuice1.jpg" alt="">
+      </div>
+      <div class="sm:col-start-3">
+        <img src="../assets/img2/BeetleJuice2.jpg" alt="">
+      </div>
+      <div class="sm:col-start-2">
+        <img src="../assets/img2/beetlefish-cadre.jpg" alt="">
+      </div> -->
+
+    
+      <!-- <div class="sm:col-start-1">
         <img src="../assets/img2/les-volants1.jpg" alt="">
       </div>
       <div>
+          <img src="../assets/img2/Le volant.jpg.jpg" alt="">
+        </div>
+      <div>
         <img src="../assets/img2/les-volants2.jpg" alt="">
-      </div>
-      <div>
-        <img src="../assets/img2/beetleJuice1.jpg" alt="">
-      </div>
-      <div>
-        <img src="../assets/img2/BeetleJuice2.jpg" alt="">
-      </div>
-      <div>
-        <img src="../assets/img2/bisou-au-soleil.jpg" alt="">
-      </div>
-      <div>
-        <img src="../assets/img2/Les-bavards.jpg" alt="">
-      </div>  -->
+      </div> -->
+
+
+      <!-- <div>
+          <img src="../assets/img2/les-bavards-cadre.jpg" alt="">
+        </div>
+        <div>
+          <img src="../assets/img2/bisou-volant-cadre.jpg" alt="">
+        </div> -->
       </div>
 
-      <div class="flex flex-col sm:flex-row max-w-4xl flex-1">
-    
-      <div>
-        <img src="../assets/img2/lenvolée-cadre.jpg" alt="">
+
+      <div class="flex flex-col sm:flex-row justify-center">
+        <div>
+          <img class="w-96" src="../assets/img2/lenvolée-cadre.jpg" alt="">
+        </div>
+        <div>
+          <img class="w-96" src="../assets/img2/perroquet-cadre.jpg" alt="">
+        </div>
       </div>
-      <div class="grid">
-        <img class="lg:col-start-3" src="../assets/img2/perroquet-cadre.jpg" alt="">
+
+      <div class="flex flex-col sm:flex-row sm:justify-center">
+        <div>
+          <img class="w-64 mr-36" src="../assets/img2/BeetleJuice2.jpg" alt="">
+        </div>
+        <div>
+          <img class="w-64 ml-36" src="../assets/img2/beetleJuice1.jpg" alt="">
+        </div>
       </div>
-    </div>
+
+      <div class="flex sm:justify-center">
+        <img class="w-96" src="../assets/img2/beetlefish-cadre.jpg" alt="">
+      </div>
+
+      <div class="container max-w-xl lg:max-w-4xl mx-auto px-4 py-8 gap-y-10 gap-x-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div>
+          <img src="../assets/img2/Les-volants1.jpg" alt="">
+        </div>
+        <div>
+          <img src="../assets/img2/Le volant.jpg.jpg" alt="">
+        </div>
+        <div>
+          <img src="../assets/img2/Les-volants2.jpg" alt="">
+        </div>
+      </div>
+
+      
+      <div class="flex flex-col sm:flex-row justify-center">
+        <div>
+          <img class="w-96" src="../assets/img2/les-bavards-cadre.jpg" alt="">
+        </div>
+        <div>
+          <img class="w-96" src="../assets/img2/bisou-volant-cadre.jpg" alt="">
+        </div>
+      </div> 
       
   
       <the-card
