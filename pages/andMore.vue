2203595c20ffcdbb8382c26b93f8c60c56b5efa9
Flora <flora.boularan@jack.agency>
2023-02-28 12:25:59 +0100

    wip add pages flowers / showcases

--- pages/andMore.vue
+++ pages/andMore.vue
@@ -4,13 +4,17 @@
 
     <h1 class="text-center my-12 font-thin text-2xl tracking-widest">Mais aussi ...</h1>
 
-    <p class="text-center text-xl font-thin tracking-widest mx-24">Pour illuminer vos évênements, sublimer vos marchandises ou encore habiller votre intérieur je propose également des objets de décorations et des réalisations scénographiques.</p>
+    <p class="text-center text-base text-center font-thin tracking-widest mx-12 lg:mx-96 mb-12">Pour illuminer vos évênements, sublimer vos marchandises ou encore habiller votre intérieur je propose également des objets de décorations et des réalisations scénographiques.</p>
 
-    <div class="container max-w-xl lg:max-w-4xl mx-auto gap-x-24 grid grid-cols-1 sm:grid-cols-2">
-        <img src="../assets/img2/LaMoula.jpg" alt="">
-        <NuxtLink class="row-start-2 col-start-1">En savoir plus</NuxtLink>
+    <div class="container max-w-md lg:max-w-2xl mx-auto gap-x-44 grid grid-cols-1 sm:grid-cols-2">
+        <img class="row-start-1 sm:mt-8" src="../assets/img3/les-fleurs.jpg" alt="">
+        <h2 class="row-start-2 font-thin italic text-xl tracking-widest mx-auto">Compositions florales</h2>
+        <NuxtLink to="flowers" class="row-start-3 my-8 border mx-auto py-3 px-9 bg-black font-thin text-white tracking-wider">Découvrir</NuxtLink>
 
-        <img src="../assets/img2/LaMoula.jpg" alt="">
-        <NuxtLink class="row-start-4 sm:row-start-2 sm:col-start-2">En savoir plus</NuxtLink>
+        <img class="row-start-4 sm:row-start-1 sm:col-start-2 mt-20 sm:mt-12" src="../assets/img3/vitrine.jpg" alt="">
+        <h2 class="row-start-5 sm:row-start-2 sm:col-start-2 font-thin italic text-xl tracking-widest mx-auto">Vitrines</h2>
+        <NuxtLink to="showcases" class="row-start-6 sm:row-start-3 sm:col-start-2 my-8 border mx-auto py-3 px-9 bg-black font-thin text-white tracking-wider">Découvrir</NuxtLink>
     </div>
+
+    <the-footer/>
 </template>
---
+++ pages/flowers.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <the-header/>
+
+    <h1 class="text-center my-12 font-thin text-2xl tracking-widest">Compositions florales</h1>
+
+    <div class="container max-w-xl lg:max-w-2xl mx-auto px-4 gap-x-8 gap-y-8 grid grid-cols-1 sm:grid-cols-2">
+        <!--lIGNE 1-->
+        <img src="../assets/flowers/fleurs-tissus.jpg" alt="">
+        <img src="../assets/flowers/bouquet-tissus.jpg" alt="">
+
+        <!--LIGNE2-->
+        <img src="../assets/flowers/fleurs-tissus.jpg" alt="">
+        <span class="font-thin text-lg tracking-wider text-center my-auto">Bouquets <br>à suspendre <br>et fleurs XXL en tissu. <br>Pétales coupées, <br>entoilées <br>et assemblées <br>à la main.</span>
+
+        <!--LIGNE 3-->
+        <img src="../assets/flowers/suspendu1.jpg" alt="">
+        <img src="../assets/flowers/suspendu2.jpg" alt="">
+
+        <!--LIGNE 4-->
+        <img class="sm:col-span-2" src="../assets/flowers/couronne.jpg" alt="">
+    </div>
+
+    <the-footer/>
+
+</template>
